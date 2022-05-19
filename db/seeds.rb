7bf6faae47823a954d1a348914f3a8bade6e1c88
Aurore Rigault <rigault.aurore@gmail.com>
2022-05-19 15:25:10 +0200

    green rake

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+List.destroy_all
+Movie.destroy_all
+
+25.times do
+  Movie.create(
+    title: Faker::Movie.title,
+    overview: Faker::Movie.quote,
+    poster_url: Faker::LoremFlickr.image,
+    rating: Faker::Number.number(digits: 1)
+  )
+end
+
+
+5.times do
+  List.create!(
+    name: Faker::Fantasy::Tolkien.location
+  )
+end
+
+List.all.each do |list|
+  5.times do
+    Bookmark.create!(
+      comment: Faker::Fantasy::Tolkien.poem,
+      list: list,
+      movie: Movie.all.sample
+    )
+  end
+end
